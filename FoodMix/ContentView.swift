a200646c1dfdc6d64bb72c13fcc87b73e4ed3feb
Nguyên Trần <dnstylish@gmail.com>
2022-03-01 14:00:00 +0700

    Initial Commit

---
+++ FoodMix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodMix
+//
+//  Created by Yuan on 30/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodMix/FoodMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMixApp.swift
+//  FoodMix
+//
+//  Created by Yuan on 30/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
