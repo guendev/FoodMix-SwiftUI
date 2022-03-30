bec7981936208bbeff91ee64547e43216505edff
Nguyên Trần <dnstylish@gmail.com>
2022-03-30 20:26:03 +0700

    update project struct

--- FoodMix/ContentView.swift
+++ FoodMix/ContentView.swift
@@ -1,21 +1,35 @@
 //
 //  ContentView.swift
-//  FoodMix
+//  Shared
 //
-//  Created by Yuan on 30/03/2022.
+//  Created by Yuan on 24/02/2022.
 //
 
 import SwiftUI
+import Introspect
 
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel: AppViewModel = AppViewModel()
+            
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            MainView()
+                .background(Color("Background").ignoresSafeArea())
+                .introspectNavigationController { navi in
+                    navi.navigationBar.isHidden = true
+                }
+        }
+        .environmentObject(viewModel)
+        .environment(\.authKey, viewModel.auth)
+        .environment(\.currentUserKey, viewModel.user)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
     }
 }
--- FoodMix/FoodMixApp.swift
+++ FoodMix/FoodMixApp.swift
@@ -2,16 +2,25 @@
 //  FoodMixApp.swift
 //  FoodMix
 //
-//  Created by Yuan on 30/03/2022.
+//  Created by Yuan on 06/03/2022.
 //
 
 import SwiftUI
+import Firebase
+
 
 @main
 struct FoodMixApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
